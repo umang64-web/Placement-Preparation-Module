82a90a621da71cc54bfc42444e2e4a0d98fdd928
uamng aggarwal <umangaggarwal64@gmail.com>
2022-07-31 02:56:50 +0530

    leetcode

---
+++ 31.next-permutation.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=31 lang=cpp
+ *
+ * [31] Next Permutation
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i,j;
+        for(i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1])
+                break;
+        }
+        if(i<0)
+            reverse(nums.begin(),nums.end());
+        else{
+            for(j=nums.size()-1;j>=i;j--){
+                if(nums[j]>nums[i])
+                    break;
+            }
+            swap(nums[i],nums[j]);
+            reverse(nums.begin()+i+1,nums.end());
+        }
+    }
+};
+// @lc code=end
+
